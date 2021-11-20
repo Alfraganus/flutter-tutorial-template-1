e1bbc57f6afb6276c9fce7c1aaf3e49e8ab994f7
Gulomjon Sulaymonov <uzpsychologist@gmail.com>
2021-11-20 16:46:54 +0500

    test2

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ import 'package:flutter_template/screens/home/products.dart';
 void main() {
   runApp(MyApp());
 }
-
+ 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
