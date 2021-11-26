178d1d62d8cdfb5414049b13ea82b4a018ff47b2
Gulomjon Sulaymonov <uzpsychologist@gmail.com>
2021-11-26 21:54:50 +0500

    detail page

---
+++ lib/screens/detail_product/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class DetailProduct extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(15),
+      child: MaterialApp(
+          title: 'Detail product',
+          debugShowCheckedModeBanner:false,
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+          ),
+          home:Scaffold (
+            backgroundColor: Colors.white,
+            appBar: AppBar(
+              backgroundColor: Colors.white,
+              foregroundColor: Colors.black,
+              elevation: 0.0,
+              bottomOpacity: 0.0,
+              leading: IconButton(
+                icon: const Icon(Icons.arrow_back,size: 25,),
+                alignment:Alignment.centerLeft,
+                tooltip: 'Show Snackbar',
+                onPressed: null,
+              ),
+              title: Text('Detail product',style: TextStyle(
+                  fontSize: 35,
+                  color: Colors.black87,
+                  fontWeight: FontWeight.bold
+              ),
+                textAlign: TextAlign.center,
+              ),
+              actions: <Widget>[
+                IconButton(
+                    icon: Icon(Icons.more_vert,
+                      color: Colors.black87,
+                      size: 30,
+                    ), onPressed: () {}),
+              ],
+
+            ),
+            body: Column(
+              children: [
+
+              ].map((e) => Padding(padding: EdgeInsets.only(top: 20), child: e)).toList(),
+            ),
+          )
+      ),
+    );
+  }
+}
--- lib/screens/home/products.dart
+++ lib/screens/home/products.dart
@@ -1,75 +1,83 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_template/screens/detail_product/main.dart';
 
 class Products extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      alignment: Alignment.centerLeft,
-      children: [
-      Image.network('https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg',
-      width:  MediaQuery.of(context).size.width*0.37,
-      height: MediaQuery.of(context).size.height*0.20,
-      alignment: Alignment.topLeft,
-      fit: BoxFit.cover,
-    ),
-    Padding(
-      padding: EdgeInsets.only(left: MediaQuery.of(context).size.width*0.38),
-      child: Container(
-        width: double.infinity,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                width:250,
-                child: Text('Minavi Headseat Pro Gaming',
-                  textAlign: TextAlign.left,
-                  softWrap: true,
-                  style:TextStyle(
-                    fontSize: 22,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-            Row(
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context).push(MaterialPageRoute(
+            builder: (context) => DetailProduct())
+        );
+        },
+      child: Stack(
+        alignment: Alignment.centerLeft,
+        children: [
+        Image.network('https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg',
+        width:  MediaQuery.of(context).size.width*0.37,
+        height: MediaQuery.of(context).size.height*0.20,
+        alignment: Alignment.topLeft,
+        fit: BoxFit.cover,
+      ),
+      Padding(
+        padding: EdgeInsets.only(left: MediaQuery.of(context).size.width*0.38),
+        child: Container(
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text('\$30.99',
-                  textAlign: TextAlign.left,
-                  softWrap: true,
-                  style:TextStyle(
-                    fontSize: 22,
-                    fontWeight: FontWeight.bold,
+                Container(
+                  width:250,
+                  child: Text('Minavi Headseat Pro Gaming',
+                    textAlign: TextAlign.left,
+                    softWrap: true,
+                    style:TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ),
-                Padding(
-                  padding: EdgeInsets.only(right: 15),
-                ),
-                Text('\$10.00',
-                  textAlign: TextAlign.left,
-                  softWrap: true,
-                  style:TextStyle(
-                    fontSize: 14,
-                    decoration:TextDecoration.lineThrough,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ],
-            ),
               Row(
                 children: [
-                  Icon(Icons.star,color: Colors.yellow,),
-                  Text('(2368 Reviews)'),
-                  Expanded(
-                      child: Icon(Icons.favorite_border,size: 35,)
-                  )
-                ]
-              )
-            ].map((e) => Padding(padding: EdgeInsets.only(top: 15), child: e)).toList(),
-          ),
+                  Text('\$30.99',
+                    textAlign: TextAlign.left,
+                    softWrap: true,
+                    style:TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(right: 15),
+                  ),
+                  Text('\$10.00',
+                    textAlign: TextAlign.left,
+                    softWrap: true,
+                    style:TextStyle(
+                      fontSize: 14,
+                      decoration:TextDecoration.lineThrough,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+                Row(
+                  children: [
+                    Icon(Icons.star,color: Colors.yellow,),
+                    Text('(2368 Reviews)'),
+                    Expanded(
+                        child: Icon(Icons.favorite_border,size: 35,)
+                    )
+                  ]
+                )
+              ].map((e) => Padding(padding: EdgeInsets.only(top: 15), child: e)).toList(),
+            ),
+        ),
       ),
-    ),
-      ],
+        ],
+      ),
     );
   }
 }
